8abb899d90cf89461b14cf81e13df75164298b3e
Yanis <janis.sci@gmail.com>
2017-09-09 23:40:58 +0200

    Setup react

---
+++ src/js/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class extends React.Component {
+  constructor () {
+    super();
+  }
+  render() {
+    return (
+      <h1>Hello, React!</h1>
+    );
+  }
+}
